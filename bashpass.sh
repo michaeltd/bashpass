f2f3d2034f2bacd9b3004059d1f81c4493087043
MichaelTd <tsouchlarakis@gmail.com>
2018-11-25 16:51:04 +0200

    pw-strength

--- bashpass.sh
+++ bashpass.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # bashpass.sh password management in ~50 lines.
 
-declare db="$(cd $(dirname ${BASH_SOURCE[0]})&& pwd)/${1:-git.db3}" dm em un pw cm pr hm act="ac"
-declare -a op=( "${grey}Quit${reset}" "${red}New${reset}" "${green}Find${reset}" "${magenta}All${reset}" "${yellow}sqlite${reset}" "${cyan}csv${reset}" "${blue}help${reset}" ) desc=( "exit this menu" "gathter details to generate a new password" "search records by domain" "print all records" "start a sqlite session against your db" "prompt for a csv file to import (eg: test.csv)" "print this message" ) cmd="sqlite3 ${db}"
+declare db="${1:-git.db3}" dm em un pw cm pr hm act="ac"
+declare -a op=( "${grey}Quit${reset}" "${red}New${reset}" "${green}Find${reset}" "${magenta}All${reset}" "${yellow}sqlite${reset}" "${cyan}csv${reset}" "${blue}help${reset}" )
+declare -a desc=( "exit this menu" "gathter details to generate a new password" "search records by domain" "print all records" "start a sqlite session against your db" "prompt for a csv file to import (eg: test.csv)" "print this message" )
+declare -a cmd="sqlite3 -line ${db}"
 
 if [[ (! -x "$(which sha512sum 2> /dev/null)") || (! -x "$(which sqlite3 2> /dev/null)") || (! $(${cmd} "select * from ${act};" 2> /dev/null)) ]]; then
-  printf "Need sha512sum, sqlite3 and a working db to function.\nIf sha512sum and sqlite3 are in place,\nRun 'sqlite3 git.db3 < ac.sql && bashpass.sh'\nin this directory.\n"
+  printf "Need sha512sum, sqlite3 and a working db to function.\nIf sha512sum and sqlite3 are in your path,\nRun 'sqlite3 my.db3 < ac.sql && bashpass.sh my.db3'\nfrom this directory: $(pwd)\n"
   exit 1
 fi
 
@@ -18,19 +20,19 @@ pr+="${bold}Choose[0-$((${#op[@]}-1))]:${reset}"
 hm+="\naccounts table format is as follows:\n$(${cmd} .schema)\n"
 
 function gatherdetails { # Get some data, hash a password
-  while  [[ -z "${dm}" || -z "${em}" || -z "${un}" || -z "${cm}" ]]; do
-    if   [[ -z "${dm}" ]]; then read -p "Domain? " dm
+  while [[ -z "${dm}" || -z "${em}" || -z "${un}" || -z "${cm}" ]]; do
+    if [[ -z "${dm}" ]]; then read -p "Domain? " dm
     elif [[ -z "${em}" ]]; then read -p "Email? " em
     elif [[ -z "${un}" ]]; then read -p "Username? " un
     elif [[ -z "${cm}" ]]; then read -p "Comment? " cm
     fi
   done
-  pw=$(echo "${RANDOM} ${dm} ${RANDOM} ${em} ${RANDOM} ${un} ${RANDOM} ${cm} ${RANDOM}"|sha512sum)
+  pw="$(head /dev/urandom|tr -dc 'a-zA-Z0-9~!@#$%^&*_-'|head -c 64)"
 }
 
 function writerecord {
-  ${cmd} "insert into ${act} values('${dm//:/\:}', '${em}', '${un}', '${pw//[![:alnum:]]}', '${cm}');"
-  sqlite3 -line "${db}" "select * from ${act} where dm like '${dm}';"
+  ${cmd} "insert into ${act} values('${dm//:/\:}', '${em}', '${un}', '${pw}', '${cm}');"
+  ${cmd} "select * from ${act} where dm like '${dm}';"
   unset dm em un pw cm
 }
 
@@ -39,8 +41,8 @@ while :; do
   case "${ui}" in
     0) break;;
     1) gatherdetails && writerecord;;
-    2) read -p "Enter domain to look for: " dm; sqlite3 -line "${db}" "select * from ${act} where dm like '%${dm}%';"; unset dm;;
-    3) sqlite3 -line "${db}" "select rowid as id, * from ${act};";;
+    2) read -p "Enter domain to look for: " dm;${cmd} "select rowid as id,* from ${act} where dm like '%${dm}%';"; unset dm;;
+    3) ${cmd} "select rowid as id, * from ${act};";;
     4) ${cmd};;
     5) read -p "Enter a csv file: " csvf; sqlite3 -csv "${db}" ".import ${csvf} ${act}";;
     6) printf "${hm[@]}\n";;
