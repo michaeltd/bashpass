a00645997961f3cd92706c29dbe55748e740b9fa
MichaelTd <tsouchlarakis@gmail.com>
2019-03-07 13:53:42 +0200

    launcher-fiddling

--- bashpass-launcher.sh
+++ bashpass-launcher.sh
@@ -2,4 +2,6 @@
 
 SDN="$(cd $(dirname ${BASH_SOURCE[0]})&& pwd)"
 
-xterm -e "${SDN}/bashpass.sh" "${@}" &
+TRM=$(which terminology||which st||which konsole||which gnome-terminal||which xfce-4-terminal||which terminator||which sakura||which termite||which tilix||which urxvt||which xterm)
+
+${TRM} -e "${SDN}/bashpass.sh" "${@}" &
