90246432c244425ec2948387110947bfecae96ae
michaeltd <tsouchlarakis@gmail.com>
2019-11-22 10:17:35 +0200

    what the hell happened here

--- bashpass.sh
+++ bashpass.sh
@@ -2,12 +2,32 @@
 #
 # bashpass/bashpass.sh Xdialog/dialog/terminal assisted password management.
 
+if [[ ! -t 1 ]]; then
+    # Don't run in anything other than a terminal (or tty)!!!
+    msg="You'll need to run $0 in a terminal!"
+
+    notify-send "${msg}" || \
+        Xdialog --title "Error" --infobox "${msg}" 0 0 30000 || \
+        xmessage -nearmouse -timeout 30 "${msg}" ||
+        printf "${msg}\n"
+
+    exit 1
+fi
+
 # Xdialog/dialog
 export XDIALOG_HIGH_DIALOG_COMPAT=1 XDIALOG_FORCE_AUTOSIZE=1 XDIALOG_INFOBOX_TIMEOUT=5000 XDIALOG_NO_GMSGS=1
 declare DIALOG_OK=0 DIALOG_CANCEL=1 DIALOG_HELP=2 DIALOG_EXTRA=3 DIALOG_ITEM_HELP=4 DIALOG_ESC=255
 declare SIG_NONE=0 SIG_HUP=1 SIG_INT=2 SIG_QUIT=3 SIG_KILL=9 SIG_TERM=15
 
 #link free (S)cript: (D)ir(N)ame, (B)ase(N)ame.
+# if [[ -L "${BASH_SOURCE[0]}" ]]; then
+#     declare SDN="$(cd $(dirname $(readlink ${BASH_SOURCE[0]}))&& pwd -P)"
+#     declare SBN="$(basename $(readlink ${BASH_SOURCE[0]}))"
+# else
+#     declare SDN="$(cd $(dirname ${BASH_SOURCE[0]})&& pwd -P)"
+#     declare SBN="$(basename ${BASH_SOURCE[0]})"
+# fi
+# Via `realpath` as no need for link checking.
 declare SDN="$(cd $(dirname $(realpath ${BASH_SOURCE[0]})) && pwd -P)"
 declare SBN="$(basename $(realpath ${BASH_SOURCE[0]}))"
 
@@ -59,7 +79,6 @@ declare MUTEX="${SDN}/.${SBN}.${BNDB}.MUTEX"
 
 do_quit() {
     # gpg2 --batch --yes --quiet --default-recipient-self --output "${DB}.asc" --encrypt "${DB}"
-    # gpg2 --default-recipient-self --output "${DB}.asc" --encrypt "${DB}"
     # shred --verbose --zero --remove --iterations=30 {"${DB}","${TF}","${MUTEX}"}
 
     # Upon successfull encryption ONLY shred files
@@ -91,10 +110,11 @@ check_decrypt() {
 
 # SQL or die.
 check_sql() {
-    if ! ${DCM[@]} "SELECT * FROM ${ACT} ORDER BY rowid ASC;" &> /dev/null; then
-        printf "${bold}Need a working db to function.${reset}\n Follow the instructions from here:\n ${underline}https://github.com/michaeltd/bashpass${reset}\n" >&2
-        return 1
-    fi
+        if ! ${DCM[@]} "SELECT * FROM ${ACT} ORDER BY rowid ASC;" &> /dev/null; then
+            printf "${bold}Need a working db to function.${reset}\n Follow the instructions from here:\n ${underline}https://github.com/michaeltd/bashpass${reset}\n" >&2
+            return 1
+        fi
+
 }
 
 # Generate PassWord
@@ -323,7 +343,7 @@ main() {
             ${DIALOG_CANCEL}) do_quit ;;
             ${DIALOG_HELP}) usage ;;
             ${DIALOG_ESC}) do_quit ;;
-        esac
+            esac
     done
 }
 
--- install.sh
+++ install.sh
@@ -2,51 +2,49 @@
 #
 # bashpass/install.sh - Install bashpass.sh
 
-# link free (S)cript (D)ir(N)ame, (B)ase(N)ame, (F)ull (N)ame.
-if [[ -L "${BASH_SOURCE[0]}" ]]
-then
-    declare SDN="$(cd $(dirname $(readlink ${BASH_SOURCE[0]}))&& pwd -P)"
-    declare SBN="$(basename $(readlink ${BASH_SOURCE[0]}))"
-    declare SFN="${SDN}/${SBN}"
-else
-    declare SDN="$(cd $(dirname ${BASH_SOURCE[0]})&& pwd -P)"
-    declare SBN="$(basename ${BASH_SOURCE[0]})"
-    declare SFN="${SDN}/${SBN}"
+if [[ ! -t 1 ]]; then
+    echo "You'll need to run $0 in a terminal"
+    exit 1
 fi
+
+[[ ! $(command -v sqlite3) ]] && printf "You need SQLite3 installed.\n" >&2 && exit 1
+
+[[ ! $(command -v gpg2) ]] && printf "You need GNU Privacy Guard v2 (gnupg) installed.\n" >&2 && exit 1
+
+# link free (S)cript (D)ir(N)ame, (B)ase(N)ame, (F)ull (N)ame.
+# if [[ -L "${BASH_SOURCE[0]}" ]]; then
+#     declare SDN="$(cd $(dirname $(readlink ${BASH_SOURCE[0]}))&& pwd -P)"
+#     declare SBN="$(basename $(readlink ${BASH_SOURCE[0]}))"
+# else
+#     declare SDN="$(cd $(dirname ${BASH_SOURCE[0]})&& pwd -P)"
+#     declare SBN="$(basename ${BASH_SOURCE[0]})"
+# fi
+# Via `realpath` as no need for link checking.
+declare SDN="$(cd $(dirname $(realpath ${BASH_SOURCE[0]})) && pwd -P)"
+declare SBN="$(basename $(realpath ${BASH_SOURCE[0]}))"
 
 DB="${1:-git.db3}"
 
-if [[ "${DB}" != *.db3 ]]
-then
-   DB+=".db3"
+if [[ "${DB}" != *.db3 && "${DB}" != *.sqlite ]]; then
+    DB="${SDN}/${DB}.sqlite"
+else
+    DB="${SDN}/${DB}"
 fi
 
-DB="${SDN}/${DB}"
-
-printf "  This script will:\n \
-  1. Make a \${HOME}/bin dir if there isn't one. \n \
-  2. Update your \$PATH env var with your ~/bin dir in ~/.bashrc \n \
-  3. Link bp-launch.sh in ~/bin \n \
-  4. Make a ${DB##*/} file ... \n \
-  5. encrypt it to ${DB##*/}.asc \n \
-  6. Execute bp-launch.sh ${DB##*/} \n"
+printf " This script will:\n \
+ 1. Make a ${DB##*/} file ... \n \
+ 2. encrypt it to ${DB##*/}.asc \n \
+ 3. Execute bashpass.sh ${DB##*/} \n"
 
 read -p "Continue? [Y/n]:" resp
 
 [[ ${resp:-y} == [Nn]* ]] && exit 1
 
-mkdir ${HOME}/bin
-
-printf "export PATH+=\":${HOME}/bin\"\n" >> ~/.bashrc
-
-export PATH+=":${HOME}/bin"
-
-ln -sf ${SDN}/bp-launch.sh ${HOME}/bin/
-
 sqlite3 "${DB}" < ac.sql
 
-gpg2 --batch --yes --quiet --default-recipient-self --output "${DB}.asc" --encrypt "${DB}"
+#gpg2 --batch --yes --quiet --default-recipient-self --output "${DB}.asc" --encrypt "${DB}"
+gpg2 --default-recipient-self --output "${DB}.asc" --encrypt "${DB}"
 
-bp-launch.sh "${DB##*/}"
+${SDN}/bashpass.sh "${DB##*/}"
 
-printf "From now on you'll be able to call bashpass.sh with: bp-launch.sh ${DB}\n" >&2
+printf "From now on you'll be able to call bashpass.sh with: bashpass.sh %s\n" "${DB##*/}" >&2
