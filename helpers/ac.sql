64c0c75530e69e04837ccb69c5a74e21d990665c
michaeltd <tsouchlarakis@gmail.com>
2020-05-31 13:59:25 +0300

    1590922765

---
+++ helpers/ac.sql
@@ -0,0 +1,11 @@
+-- bashpass/ac.sql
+--
+-- Initialize a demo SQLite3 db. usage: sqlite3 git.db3 < ac.sql
+
+CREATE TABLE IF NOT EXISTS ac(dm varchar(100),em varchar(100),un varchar(100),pw varchar(256),cm varchar(100));
+
+INSERT OR REPLACE INTO ac VALUES('https://facebook.com/','adummy@email.com' ,'emanresu','o6vA\*2HSt4a_h!]','foobuzzbar');
+INSERT OR REPLACE INTO ac VALUES('https://www.google.com/','mail@gmail.com','googleuser','LL2Pto!vR&d!Y&I@','fizzbuzzbar');
+INSERT OR REPLACE INTO ac VALUES('https://twitter.com/','tsirp@twitter.com','tsirpinator','I8;l[\D<Qf~lmad]','foobarbuzz');
+INSERT OR REPLACE INTO ac VALUES('https://instagram.com/','selfindulgence@isgreat.me','notjustabimbo','fINjf>%lxVI{1ict','memyselfni');
+INSERT OR REPLACE INTO ac VALUES('https://pinterest.com/','looking@pictures.jif','artzor','OBXpzhQvaC({UFGz','daliforprez');
