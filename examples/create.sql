bd0d7be1b112146f9ce2d6a0ef7e8f6f38717681
michaeltd <tsouchlarakis@gmail.com>
2021-03-01 10:33:07 +0200

    1614587587

--- examples/create.sql
+++ examples/create.sql
@@ -1,4 +1,5 @@
 --
+--
 -- Initialize a demo SQLite3 db. usage: sqlite3 git.db3 < create.sql
 
 CREATE TABLE IF NOT EXISTS ac(dm varchar(100),em varchar(100),un varchar(100),pw varchar(256),cm varchar(100));
