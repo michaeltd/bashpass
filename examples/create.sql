9b4ae7f44047b667e4cf79034a75b251b9f1492c
michaeltd <tsouchlarakis@gmail.com>
2021-08-20 12:50:52 +0300

    1629453052

--- examples/create.sql
+++ examples/create.sql
@@ -2,7 +2,7 @@
 --
 -- Initialize a demo SQLite3 db. usage: sqlite3 git.db3 < create.sql
 
-CREATE TABLE IF NOT EXISTS ACCOUNTS(DOMAIN varchar(100) NOT NULL,EMAIL varchar(100) NOT NULL,USERNAME varchar(100) NOT NULL,PASSWORD varchar(256) NOT NULL,COMMENT varchar(100), PRIMARY KEY(DOMAIN,EMAIL));
+CREATE TABLE IF NOT EXISTS ACCOUNTS(DM nchar(100) NOT NULL,EM nchar(100) NOT NULL,UN nchar(100) NOT NULL,PW nchar(256) NOT NULL,CM nchar(100), PRIMARY KEY(DM,EM));
 
 INSERT OR REPLACE INTO ACCOUNTS VALUES('https://facebook.com/','adummy@email.com' ,'emanresu','o6vA\*2HSt4a_h!]','foobuzzbar');
 INSERT OR REPLACE INTO ACCOUNTS VALUES('https://www.google.com/','mail@gmail.com','googleuser','LL2Pto!vR&d!Y&I@','fizzbuzzbar');
