67492ed308a7fea9e204f63c074480d93a0aab7c
michaeltd <tsouchlarakis@gmail.com>
2021-08-20 12:15:31 +0300

    1629450931

--- examples/create.sql
+++ examples/create.sql
@@ -2,10 +2,10 @@
 --
 -- Initialize a demo SQLite3 db. usage: sqlite3 git.db3 < create.sql
 
-CREATE TABLE IF NOT EXISTS ac(dm varchar(100),em varchar(100),un varchar(100),pw varchar(256),cm varchar(100));
+CREATE TABLE IF NOT EXISTS ACCOUNTS(DOMAIN varchar(100) NOT NULL,EMAIL varchar(100) NOT NULL,USERNAME varchar(100) NOT NULL,PASSWORD varchar(256) NOT NULL,COMMENT varchar(100), PRIMARY KEY(DOMAIN,EMAIL));
 
-INSERT OR REPLACE INTO ac VALUES('https://facebook.com/','adummy@email.com' ,'emanresu','o6vA\*2HSt4a_h!]','foobuzzbar');
-INSERT OR REPLACE INTO ac VALUES('https://www.google.com/','mail@gmail.com','googleuser','LL2Pto!vR&d!Y&I@','fizzbuzzbar');
-INSERT OR REPLACE INTO ac VALUES('https://twitter.com/','tsirp@twitter.com','tsirpinator','I8;l[\D<Qf~lmad]','foobarbuzz');
-INSERT OR REPLACE INTO ac VALUES('https://instagram.com/','selfindulgence@isgreat.me','notjustabimbo','fINjf>%lxVI{1ict','memyselfni');
-INSERT OR REPLACE INTO ac VALUES('https://pinterest.com/','looking@pictures.jif','artzor','OBXpzhQvaC({UFGz','daliforprez');
+INSERT OR REPLACE INTO ACCOUNTS VALUES('https://facebook.com/','adummy@email.com' ,'emanresu','o6vA\*2HSt4a_h!]','foobuzzbar');
+INSERT OR REPLACE INTO ACCOUNTS VALUES('https://www.google.com/','mail@gmail.com','googleuser','LL2Pto!vR&d!Y&I@','fizzbuzzbar');
+INSERT OR REPLACE INTO ACCOUNTS VALUES('https://twitter.com/','tsirp@twitter.com','tsirpinator','I8;l[\D<Qf~lmad]','foobarbuzz');
+INSERT OR REPLACE INTO ACCOUNTS VALUES('https://instagram.com/','selfindulgence@isgreat.me','notjustabimbo','fINjf>%lxVI{1ict','memyselfni');
+INSERT OR REPLACE INTO ACCOUNTS VALUES('https://pinterest.com/','looking@pictures.jif','artzor','OBXpzhQvaC({UFGz','daliforprez');
