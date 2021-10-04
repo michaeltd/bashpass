7d9688fdf732a5624b7027869b676be01d257bf8
michaeltd <tsouchlarakis@gmail.com>
2021-10-04 10:45:11 +0300

    1633333511

--- examples/create.sql
+++ examples/create.sql
@@ -1,10 +1,11 @@
-1,https://liveleak.com/,traffic@accidents.io,crashpornaddict,S^G~cgKwQVq)0XJf,roadrageisgoodforu
-2,https://wikipedia.org/,atruescolar@wikia.org,harrypotter,%KUpr@ROJwQ*Bd_+,voldemortdidnothingwrong
-3,https://www.twitch.com/,streamer@twitching.com,sofaking,!HuiV7DW9d+*.7Kx,leroyjeeenkings
-4,https://www.reddit.com/,reading@reader.com,geekerman,jd[d<q+;gLeCOyxj,r/RoastMe
-5,https://twitter.com/,twitbot@botroost.net,chirpinator,somerandompass,SirChirpALot
-6,https://facebook.com/,adummy@email.com,emanresu,o6vA/*2HSt4a_h!],foobuzzbar
-7,https://www.google.com/,mail@gmail.com,googleuser,LL2Pto!vR&d!Y&I@,fizzbuzzbar
-8,https://instagram.com/,selfindulgence@isgreat.me,notjustabimbo,fINjf>%lxVI{1ict,memyselfni
-9,https://pinterest.com/,looking@pictures.jif,artzor,OBXpzhQvaC({UFGz,daliforprez
-10,https://9gag.com/,me@toilet.pup,wcking,xCLRh~@dFw3jKn}1,yourawizardharry
+--
+--
+-- Initialize a demo SQLite3 db. usage: sqlite3 ../databases/sample < ./create.sql
+
+CREATE TABLE IF NOT EXISTS ACCOUNTS (DM TEXT,EM TEXT,UN TEXT,PW TEXT,CM TEXT,PRIMARY KEY(DM, EM));
+
+INSERT OR REPLACE INTO ACCOUNTS VALUES('https://facebook.com/','adummy@email.com' ,'emanresu','o6vA/*2HSt4a_h!]','foobuzzbar');
+INSERT OR REPLACE INTO ACCOUNTS VALUES('https://www.google.com/','mail@gmail.com','googleuser','LL2Pto!vR&d!Y&I@','fizzbuzzbar');
+INSERT OR REPLACE INTO ACCOUNTS VALUES('https://twitter.com/','tsirp@twitter.com','tsirpinator','I8;l[/D<Qf~lmad]','foobarbuzz');
+INSERT OR REPLACE INTO ACCOUNTS VALUES('https://instagram.com/','selfindulgence@isgreat.me','notjustabimbo','fINjf>%lxVI{1ict','memyselfni');
+INSERT OR REPLACE INTO ACCOUNTS VALUES('https://pinterest.com/','looking@pictures.jif','artzor','OBXpzhQvaC({UFGz','daliforprez');
