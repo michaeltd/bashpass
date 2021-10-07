ad02a2775c0eb8b816d78b8abfb7262e17a3706a
michaeltd <tsouchlarakis@gmail.com>
2021-10-07 10:44:36 +0300

    1633592676

--- examples/create.sql
+++ examples/create.sql
@@ -1,13 +1,11 @@
 --
 --
--- Initialize a demo SQLite3 db. usage: sqlite3 ../databases/sample < ./create.sql
+-- Initialize the default SQLite3 db.
 
--- CREATE TABLE IF NOT EXISTS ACCOUNTS (DM TEXT,EM TEXT,UN TEXT,PW TEXT,CM TEXT,PRIMARY KEY(DM, EM));
--- CREATE TABLE IF NOT EXISTS ACCOUNTS (ID INTEGER PRIMARY KEY, DM TEXT,EM TEXT,UN TEXT,PW TEXT,CM TEXT,UNIQUE(DM, EM)) WITHOUT ROWID;
-CREATE TABLE ACCOUNTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, DM TEXT, EM TEXT, UN TEXT, PW TEXT, CM TEXT, UNIQUE(DM, EM));
+CREATE TABLE ACCOUNTS (DM TEXT,EM TEXT,UN TEXT,PW TEXT,CM TEXT,PRIMARY KEY(DM, EM));
 
-INSERT INTO ACCOUNTS (DM,EM,UN,PW,CM) VALUES('https://facebook.com/','adummy@email.com' ,'emanresu','o6vA/*2HSt4a_h!]','foobuzzbar');
-INSERT INTO ACCOUNTS (DM,EM,UN,PW,CM) VALUES('https://www.google.com/','mail@gmail.com','googleuser','LL2Pto!vR&d!Y&I@','fizzbuzzbar');
-INSERT INTO ACCOUNTS (DM,EM,UN,PW,CM) VALUES('https://twitter.com/','tsirp@twitter.com','tsirpinator','I8;l[/D<Qf~lmad]','foobarbuzz');
-INSERT INTO ACCOUNTS (DM,EM,UN,PW,CM) VALUES('https://instagram.com/','selfindulgence@isgreat.me','notjustabimbo','fINjf>%lxVI{1ict','memyselfni');
-INSERT INTO ACCOUNTS (DM,EM,UN,PW,CM) VALUES('https://pinterest.com/','looking@pictures.jif','artzor','OBXpzhQvaC({UFGz','daliforprez');
+INSERT INTO ACCOUNTS VALUES('https://facebook.com/','adummy@email.com' ,'emanresu','o6vA/*2HSt4a_h!]','foobuzzbar');
+INSERT INTO ACCOUNTS VALUES('https://www.google.com/','mail@gmail.com','googleuser','LL2Pto!vR&d!Y&I@','fizzbuzzbar');
+INSERT INTO ACCOUNTS VALUES('https://twitter.com/','tsirp@twitter.com','tsirpinator','I8;l[/D<Qf~lmad]','foobarbuzz');
+INSERT INTO ACCOUNTS VALUES('https://instagram.com/','selfindulgence@isgreat.me','notjustabimbo','fINjf>%lxVI{1ict','memyselfni');
+INSERT INTO ACCOUNTS VALUES('https://pinterest.com/','looking@pictures.jif','artzor','OBXpzhQvaC({UFGz','daliforprez');
